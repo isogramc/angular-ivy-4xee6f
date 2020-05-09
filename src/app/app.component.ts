b10bb8936717bac3054163682ba93b74751d41f9
isogramc <theresa@isogramconsulting.com>
2020-05-09 17:25:29 +0200

    implemented focus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,37 @@
-import { Component, VERSION,  ElementRef, ViewChild } from '@angular/core';
+import { Component, VERSION,  ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   title = 'Angular ' + VERSION.major + ' Master Class';
-  number1: any;
-  number2: any;
-  answer: any;
+  number1: number;
+  number2: number;
+  answer: number;
   height = 54;
   width = 54;
-
   operand = '+';
 
   @ViewChild('box3', { static: false }) public box3: ElementRef;
   @ViewChild('box2', { static: false }) public box2: ElementRef;
   @ViewChild('box1', { static: false }) public box1: ElementRef;
 
+ngOnInit(){
+  setTimeout(() => this.box1.nativeElement.focus(), );
+}
+ 
+
   calculate(){
     if (this.operand === '+'){
-      this.answer = parseFloat(this.number1) + parseFloat(this.number2);
+      this.answer = this.number1 + this.number2;
     } else if (this.operand === '-') {
-      this.answer = parseFloat(this.number1) - parseFloat(this.number2);
+      this.answer = this.number1 - this.number2;
     } else if (this.operand === '/') {
-      this.answer = parseFloat(this.number1) / parseFloat(this.number2);
+      this.answer = this.number1 / this.number2;
     } else if (this.operand === '*') {
-      this.answer = parseFloat(this.number1) * parseFloat(this.number2);
+      this.answer = this.number1 * this.number2;
     }
   }
 
